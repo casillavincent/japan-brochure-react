ddc18f49f0e401775652251083c0e401d093ae3f
Vincent Casilla <73447863+mikanunu@users.noreply.github.com>
2021-02-07 01:20:55 -0800

    Added AOS

--- src/components/Accommodations.js
+++ src/components/Accommodations.js
@@ -9,7 +9,12 @@ import Capsule from "../assets/accommodations/capsule.jpg";
 // Compoenents
 import TopButton from "../components/TopButton";
 
+//AOS Library
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const Accommodations = () => {
+   AOS.init();
    return (
       <React.Fragment>
          {/* Page Title */}
@@ -20,7 +25,7 @@ const Accommodations = () => {
 
          <main className="accommodations">
             {/* Ryokan */}
-            <section className="accommodations-card">
+            <section className="accommodations-card" data-aos="slide-right">
                <img
                   className="accommodations-card__img"
                   src={`${Ryokan}`}
@@ -50,7 +55,7 @@ const Accommodations = () => {
             </section>
 
             {/* Manga Cafe */}
-            <section className="accommodations-card">
+            <section className="accommodations-card" data-aos="slide-right">
                <img
                   className="accommodations-card__img"
                   alt="Ryokan"
@@ -80,7 +85,7 @@ const Accommodations = () => {
             </section>
 
             {/* Capsule Hotel */}
-            <section className="accommodations-card">
+            <section className="accommodations-card" data-aos="slide-right">
                <img
                   className="accommodations-card__img"
                   alt="Ryokan"
@@ -110,7 +115,7 @@ const Accommodations = () => {
             </section>
 
             {/* Temple */}
-            <section className="accommodations-card">
+            <section className="accommodations-card" data-aos="slide-right" data-aos-mirror="true">
                <img
                   className="accommodations-card__img"
                   alt="Ryokan"
